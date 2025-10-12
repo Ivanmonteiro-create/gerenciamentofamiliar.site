2f09b81f28e0baf2c8d1256fc45f45c28c09d796
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-10-12 23:17:20 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,10 +8,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="pt">
-      {/* classe ativa o tema global */}
-      <body> className="theme-dark-soft">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
