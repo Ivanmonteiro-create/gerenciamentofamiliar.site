55e8a09f61d0fa44ce4aa2c856ffadd3f55a7567
Dorgivan Monteiro <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-11-09 22:24:49 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  fallbacks: {
+    document: '/offline.html',
+  },
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
