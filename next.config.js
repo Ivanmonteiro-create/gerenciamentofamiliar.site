1ed07294b8c3318292954eed0bb92652535ad235
Dorgivan Monteiro <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-11-11 01:25:37 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
